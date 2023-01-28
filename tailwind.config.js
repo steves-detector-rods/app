0b95d651cf2f3b62a5fb678da285dce0a527eac0
Cameron Bristol <cameron.bristol@handsdowndigital.com>
2023-01-28 11:53:45 -0600

    config fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/app/**/*.tsx', './src/pages/**/*.tsx'],
+	purge: ['./src/app/**/*.tsx', './src/pages/**/*.tsx', './src/components/**/*.tsx', './src/domains/**/*.tsx'],
+	mode: 'jit',
 	theme: {
 		extend: {},
 	},
