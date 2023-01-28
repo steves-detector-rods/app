5ec091e9f2475bcc9261e846e140995b0e04068c
Cameron Bristol <bulldogslucky13@users.noreply.github.com>
2023-01-28 10:37:52 -0600

    Test ci and cd pt. 2 (#2)
    * Test ci and cd
    
    * Update ignore files
    
    * File format cleanup
    
    ---------
    
    Co-authored-by: Cameron Bristol <cameron.bristol@handsdowndigital.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
 	return (
 		<main>
 			<div className="w-96 bg-blue-50">
-				<Image src="/logo-long.png" alt="Steve's Detector Rods Logo" layout="fill" objectFit="contain" />
+				<Image src="/logo-long.png" alt="Steve's Detector Rods Logo" width={200} height={175} />
 			</div>
-			<p className="text-xl bg-red">This is some home testing text</p>
+			<p className="text-xl bg-red">Testing my home page</p>
 		</main>
 	);
 }
