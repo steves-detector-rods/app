6625389d76cbeaf1a64445a41924b27f1da60cf7
Cameron Bristol <cameron.bristol@handsdowndigital.com>
2023-01-28 18:36:56 -0600

    Create mobile version of top nav

--- src/components/navigation/header.tsx
+++ src/components/navigation/header.tsx
@@ -1,11 +1,23 @@
 import Image from 'next/image';
+import { HiBars3 } from 'react-icons/hi2';
 
-export default function Header() {
+export default function Header({ onHamburgerClick = () => {} }: { onHamburgerClick?: () => void }) {
 	return (
-		<div className="justify-center items-center px-4 lg:px-32 xl:px-64 py-4">
-			<a href="/">
-				<Image src="/logo-long.png" alt="Steve's Detector Rods Logo" width={417} height={30} className="aspect-auto" />
-			</a>
+		<div className="justify-center w-full max-w-6xl px-4 py-4">
+			<div className="flex flex-row items-center space-x-2 sm:space-x-0">
+				<button className="inline sm:hidden" onClick={onHamburgerClick}>
+					<HiBars3 className="h-7 w-7" />
+				</button>
+				<a href="/">
+					<Image
+						src="/logo-long.png"
+						alt="Steve's Detector Rods Logo"
+						width={389}
+						height={28}
+						className="aspect-auto"
+					/>
+				</a>
+			</div>
 		</div>
 	);
 }
--- src/components/navigation/sub-nav/sub-nav.tsx
+++ src/components/navigation/sub-nav/sub-nav.tsx
@@ -1,12 +1,27 @@
+import { clsx } from 'utils/clsx';
 import SubNavItem from './sub-nav-item';
 
-export default function SubNav() {
+export default function SubNav({ isNavOpen }: { isNavOpen: boolean }) {
 	return (
-		<div className="w-full bg-red-800 px-4 lg:px-32 xl:px-64 py-2 space-x-8">
-			<SubNavItem href="/">Home</SubNavItem>
-			<SubNavItem href="/products">Products</SubNavItem>
-			<SubNavItem href="/about">About</SubNavItem>
-			<SubNavItem href="/blog">Blog</SubNavItem>
+		<div className={clsx('w-full bg-red-800 py-2 justify-center', isNavOpen ? 'flex' : 'hidden sm:flex')}>
+			<div className="flex flex-col sm:flex-row justify-between w-full max-w-6xl px-4 sm:space-x-4">
+				<div className="flex w-full sm:w-auto space-y-2 sm:space-y-0 sm:space-x-8 flex-col sm:flex-row">
+					<SubNavItem href="/">Home</SubNavItem>
+					<SubNavItem href="/products">Products</SubNavItem>
+					<SubNavItem href="/about">About</SubNavItem>
+					<SubNavItem href="/blog">Blog</SubNavItem>
+				</div>
+				<p className="hidden md:block text-white">
+					<span>Questions? Contact Steve: </span>
+					<a href="mailto:steve@stevesdetectorrods.com">steve@stevesdetectorrods.com</a>
+				</p>
+				<a
+					href="mailto:steve@stevesdetectorrods.com"
+					className="block md:hidden text-white text-center sm:text-start py-2 sm:py-0"
+				>
+					Contact Steve
+				</a>
+			</div>
 		</div>
 	);
 }
