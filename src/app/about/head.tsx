e991ec7a17fa01e7571264f522b3f4cc0afded68
Cameron Bristol <cbristol28@yahoo.com>
2023-01-29 10:24:27 -0600

    Add SEO optimizations for about page

--- src/app/about/head.tsx
+++ src/app/about/head.tsx
@@ -3,6 +3,10 @@ export default function Head() {
 		<>
 			<title>Steve&apos;s Detector Rods | About</title>
 			<meta content="width=device-width, initial-scale=1" name="viewport" />
+			<meta
+				name="description"
+				content="Steve's Detector Rods is ran by a detectorist, for detectorists. Learn about us and our mission here"
+			/>
 			<link rel="icon" href="/favicon.ico" />
 		</>
 	);
