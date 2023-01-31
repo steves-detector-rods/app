bf7eebcc73242f62b34e6137907f48cc49b07d4c
Cameron Bristol <cbristol28@yahoo.com>
2023-01-30 22:13:31 -0600

    Initial-footer-commit

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,11 +1,13 @@
 import { ReactNode } from 'react';
+import Footer from 'components/footer';
 import TopNav from 'components/navigation/top-nav';
 
 export default function Template({ children }: { children: ReactNode }) {
 	return (
-		<div className="flex flex-col w-full items-center">
+		<div className="flex flex-col w-full items-center min-h-screen">
 			<TopNav />
 			<div className="w-full max-w-6xl px-4 py-2 sm:py-4">{children}</div>
+			<Footer />
 		</div>
 	);
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+	return (
+		<div className="flex w-full bg-gray-200 justify-center mt-4">
+			<div className="w-full max-w-6xl px-4 py-16">
+				<div className="text-right text-sm">
+					<p>&copy; 2018-2023 - All Rights Reserved</p>
+					<p>www.stevesdetectorrods.com</p>
+					<p>Our &quot;Steve&apos;s Detector Rods&quot; carbon-fiber shafts are proudly assembled in the USA</p>
+				</div>
+			</div>
+		</div>
+	);
+}
