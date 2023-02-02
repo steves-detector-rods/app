7259cfc6e16c44ceefb0a0d15a4d4b2eac0ed0f8
Cameron Bristol <cbristol28@yahoo.com>
2023-02-01 18:13:22 -0600

    DefaultHead work

--- src/app/about/head.tsx
+++ src/app/about/head.tsx
@@ -1,13 +1,14 @@
+import DefaultHead from 'app/default-head';
+
 export default function Head() {
 	return (
 		<>
+			<DefaultHead />
 			<title>Steve&apos;s Detector Rods | About</title>
-			<meta content="width=device-width, initial-scale=1" name="viewport" />
 			<meta
 				name="description"
 				content="Steve's Detector Rods is ran by a detectorist, for detectorists. Learn about us and our mission here"
 			/>
-			<link rel="icon" href="/favicon.ico" />
 		</>
 	);
 }
--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,14 +1,15 @@
+import DefaultHead from './default-head';
+
 export default function Head() {
 	return (
 		<>
+			<DefaultHead />
 			<title>Steve&apos;s Detector Rods | Home</title>
-			<meta content="width=device-width, initial-scale=1" name="viewport" />
 			<meta
 				name="description"
 				content="Steve's Detector Rods, Your home of premium-quality carbon-fiber detector rods/shafts, sand-scoop 
 				handles, and other accessories for your detecting needs"
 			/>
-			<link rel="icon" href="/favicon.ico" />
 		</>
 	);
 }
