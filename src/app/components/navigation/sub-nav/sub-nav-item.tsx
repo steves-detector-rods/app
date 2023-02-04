94cdd1fc88643a2edaac131f9f25cb8f140c34ee
Cameron Bristol <bulldogslucky13@users.noreply.github.com>
2023-02-03 22:43:37 -0600

    Refactor nav, begin better mobile side nav (#11)

--- src/app/components/navigation/sub-nav/sub-nav-item.tsx
+++ src/app/components/navigation/sub-nav/sub-nav-item.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 export default function SubNavItem({ children, href }: PropsWithChildren<{ href: string }>) {
 	return (
-		<a href={href} className="text-white text-md align-middle w-full sm:w-auto text-center sm:text-start py-2 sm:py-0">
+		<a href={href} className="text-white text-md align-middle w-auto text-start py-2 sm:py-0">
 			{children}
 		</a>
 	);
