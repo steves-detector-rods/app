17fffa4e34cc840531199684951fb16c902210df
Cameron Bristol <cbristol28@yahoo.com>
2023-02-05 08:15:41 -0600

    Create custom checkbox component

---
+++ src/app/components/runway/icon-checkbox.tsx
@@ -0,0 +1,39 @@
+import { ReactNode } from 'react';
+
+type CustomIcon = {
+	on: ReactNode;
+	off?: ReactNode;
+};
+
+export default function IconCheckbox({
+	key,
+	text,
+	value,
+	icon,
+}: {
+	key: string;
+	value: boolean;
+	icon: { base: CustomIcon; contrast?: CustomIcon };
+	text?: string;
+}) {
+	const generatedKey = `filter-list-checkbox-${key}`;
+
+	return (
+		<span className="group flex items-center">
+			<input type="checkbox" id={generatedKey} value={String(value)} className="hidden" checked={value} readOnly />
+			<label htmlFor={generatedKey} className="hidden contrast-more:block">
+				{value ? icon.base.on : icon.base.off}
+			</label>
+			{icon.contrast ? (
+				<label htmlFor={generatedKey} className="contrast-more:hidden">
+					{value ? icon.contrast.on : icon.contrast.off}
+				</label>
+			) : null}
+			{text ? (
+				<label htmlFor={generatedKey} className="ml-3">
+					{text}
+				</label>
+			) : undefined}
+		</span>
+	);
+}
