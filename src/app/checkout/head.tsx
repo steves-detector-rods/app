3b99667b6714fe19d2053c2294acef57e6a00015
Cameron Bristol <cbristol28@yahoo.com>
2023-02-06 22:44:58 -0600

    Create checkout page

---
+++ src/app/checkout/head.tsx
@@ -0,0 +1,10 @@
+import DefaultHead from '../default-head';
+
+export default function Head() {
+	return (
+		<>
+			<DefaultHead />
+			<title>Steve&apos;s Detector Rods | Checkout</title>
+		</>
+	);
+}
