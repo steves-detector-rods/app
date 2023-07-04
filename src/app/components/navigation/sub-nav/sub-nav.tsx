7a3826eeb44386c803b9774cbf9ce8e23a09113b
Cameron Bristol <cbristol28@yahoo.com>
2023-07-04 17:27:14 -0500

    Move sub nav to portal

--- src/app/components/navigation/sub-nav/sub-nav.tsx
+++ src/app/components/navigation/sub-nav/sub-nav.tsx
@@ -1,3 +1,4 @@
+import { Portal } from '@headlessui/react';
 import { AnimatePresence, motion, MotionConfig } from 'framer-motion';
 import { MdClose } from 'react-icons/md';
 import { clsx } from 'src/app/utils/clsx';
@@ -20,39 +21,45 @@ export default function SubNav() {
 
 	return (
 		<>
-			<MotionConfig transition={{ type: 'tween', duration: 0.2 }}>
-				<AnimatePresence>
-					<motion.div
-						key={`sub-nav-modal-background-${isSubNavOpen}`}
-						initial={{ opacity: 0 }}
-						animate={{ opacity: '25%' }}
-						exit={{ opacity: 0 }}
-						className={clsx('fixed sm:hidden', 'w-full h-full bg-black', isSubNavOpen ? 'flex' : 'hidden')}
-						onClick={() => toggleSubNavOpen(false)}
-					/>
-					<motion.div
-						key={`sub-nav-${isSubNavOpen}`}
-						initial={{ x: '-100%' }}
-						animate={{ x: 0 }}
-						exit={{ x: '-100%' }}
-						className={clsx('fixed sm:hidden', 'h-full left-0', isSubNavOpen ? 'flex' : 'hidden')}
-					>
-						<div className="flex flex-col bg-red-800 min-w-[60vw] h-full px-6 py-4 justify-between">
-							<div className="flex flex-row justify-between">
-								<div className="flex flex-col space-y-2">
-									<SubNavOptions />
+			<Portal>
+				<MotionConfig transition={{ type: 'tween', duration: 0.2 }}>
+					<AnimatePresence>
+						<motion.div
+							key={`sub-nav-modal-background-${isSubNavOpen}`}
+							initial={{ opacity: 0 }}
+							animate={{ opacity: '25%' }}
+							exit={{ opacity: 0 }}
+							className={clsx(
+								'absolute top-0 left-0 sm:hidden',
+								'w-full h-full bg-black',
+								isSubNavOpen ? 'flex' : 'hidden',
+							)}
+							onClick={() => toggleSubNavOpen(false)}
+						/>
+						<motion.div
+							key={`sub-nav-${isSubNavOpen}`}
+							initial={{ x: '-100%' }}
+							animate={{ x: 0 }}
+							exit={{ x: '-100%' }}
+							className={clsx('absolute top-0 left-0 sm:hidden', 'h-full left-0', isSubNavOpen ? 'flex' : 'hidden')}
+						>
+							<div className="flex flex-col bg-red-800 min-w-[60vw] h-full px-6 py-4 justify-between">
+								<div className="flex flex-row justify-between">
+									<div className="flex flex-col space-y-2">
+										<SubNavOptions />
+									</div>
+									<button className="mt-2 text-white h-min" onClick={() => toggleSubNavOpen(false)}>
+										<MdClose className="w-5 h-5" />
+									</button>
 								</div>
-								<button className="mt-2 text-white h-min" onClick={() => toggleSubNavOpen(false)}>
-									<MdClose className="w-5 h-5" />
-								</button>
+								<a href="mailto:steve@stevesdetectorrods.com" className="text-white text-start py-2">
+									Contact Steve
+								</a>
 							</div>
-							<a href="mailto:steve@stevesdetectorrods.com" className="text-white text-start py-2">
-								Contact Steve
-							</a>
-						</div>
-					</motion.div>
-				</AnimatePresence>
-			</MotionConfig>
+						</motion.div>
+					</AnimatePresence>
+				</MotionConfig>
+			</Portal>
 			<div
 				className="hidden sm:flex sticky top-[3.75rem]
 							w-full bg-red-800 py-2 justify-center"
