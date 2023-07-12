ce47729589d1f475238c4a4e9c3c8e20bd3f4431
Cameron Bristol <bulldogslucky13@users.noreply.github.com>
2023-07-11 22:50:13 -0500

    Home-page/browse-products-clean-up (#29)
    * Fix hero section spacing
    
    * Update tabbing animation for browse products section

--- src/app/components/runway/cta-button.tsx
+++ src/app/components/runway/cta-button.tsx
@@ -3,16 +3,18 @@ import { HTMLProps, PropsWithChildren, useRef } from 'react';
 import { AriaLinkOptions, useLink } from '@react-aria/link';
 import { clsx } from 'src/app/utils/clsx';
 
-type ColorVariant = 'primary';
+type ColorVariant = 'primary' | 'secondary';
 
 const colorVariantClasses: Record<ColorVariant, string> = {
-	primary: 'bg-red-700 hover:bg-red-800',
+	primary: 'bg-red-700 hover:bg-red-800 text-white',
+	secondary: 'bg-gray-200 hover:bg-gray-300 text-black',
 };
 
-type Size = 'lg' | 'sm';
+type Size = 'sm' | 'md' | 'lg';
 
 const sizeClasses: Record<Size, string> = {
 	lg: 'px-12 sm:px-16 py-2 sm:py-3 rounded-xl text-lg sm:text-xl lg:text-2xl',
+	md: 'px-8 sm:px-12 py-1.5 sm:py-2 rounded-xl text-lg lg:text-xl',
 	sm: 'px-6 py-1.5 rounded-lg text-md lg:text-lg',
 };
 
@@ -42,7 +44,7 @@ export function CtaButtonLink({
 			ref={ref}
 			href={href}
 			target={target}
-			className={clsx(colorVariantClasses[variant], sizeClasses[size], 'transition-colors 100ms text-white', className)}
+			className={clsx(colorVariantClasses[variant], sizeClasses[size], 'transition-colors 100m', className)}
 			{...linkProps}
 		>
 			{children}
--- src/app/components/runway/section-wrapper.tsx
+++ src/app/components/runway/section-wrapper.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 import { clsx } from 'src/app/utils/clsx';
 
 export function MainSectionWrapper({ className = '', children }: PropsWithChildren<{ className?: string }>) {
-	return <main className={clsx('px-4 lg:px-12 xl:px-20 w-full', className)}>{children}</main>;
+	return <main className={clsx('px-4 lg:px-12 xl:px-20', className)}>{children}</main>;
 }
 
 export function DivSectionWrapper({ className = '', children }: PropsWithChildren<{ className?: string }>) {
