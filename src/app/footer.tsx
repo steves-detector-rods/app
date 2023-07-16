c56150653b1ff40df2a671f0e956d6f1831597b9
Cameron Bristol <cbristol28@yahoo.com>
2023-07-16 09:35:12 -0400

    Update footer copy

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
 				<div className="text-right text-sm">
 					<p>&copy; 2018-2023 - All Rights Reserved</p>
 					<p>www.stevesdetectorrods.com</p>
-					<p>Our &quot;Steve&apos;s Detector Rods&quot; carbon-fiber shafts are proudly assembled in the USA</p>
+					<p>&quot;Steve&apos;s Detector Rods&quot; carbon-fiber shafts are proudly assembled in the USA</p>
 				</div>
 			</div>
 		</div>
