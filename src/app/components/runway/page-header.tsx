c13c7709e297271fb197fe2d26bb1ea3294957b9
Cameron Bristol <cbristol28@yahoo.com>
2023-07-16 09:16:52 -0400

    Refactor page header to be more reusable

--- src/app/components/runway/page-header.tsx
+++ src/app/components/runway/page-header.tsx
@@ -1,5 +1,21 @@
 import { PropsWithChildren } from 'react';
+import { clsx } from 'src/app/utils/clsx';
 
-export default function PageHeader({ children }: PropsWithChildren<{}>) {
-	return <h1 className="text-gray-800 font-medium text-3xl text-left my-3 sm:my-4">{children}</h1>;
+interface PageHeadingProps extends React.HTMLAttributes<HTMLHeadingElement> {
+	headingLevel?: React.ElementType;
+}
+
+export default function PageHeader({
+	headingLevel = 'h1',
+	className,
+	children,
+	...props
+}: PropsWithChildren<PageHeadingProps>) {
+	const Heading = headingLevel;
+
+	return (
+		<Heading className={clsx('text-gray-800 font-medium text-3xl text-left my-3 sm:my-4', className)} {...props}>
+			{children}
+		</Heading>
+	);
 }
